cc9cc7ccd86aaa9c51b02b0a9dcbff221d7fc031
Christian Smith <christiansmithdeveloper@gmail.com>
2021-01-19 10:25:30 +1000

    removed libs and updated to use the published version

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const messaging = require('.')
-//const googleAuth = require('./googleauth.js')
 const client = new messaging.MessagesClient({ keyFile: './1608242628700.json' })
-// client.getProjectId().then(res => {
-//   console.log(res);
-// })
 client.getMessage({name: 'projects/first/messages/r8l028d8u3y0v26pdtpjes1j6671mu'}).then(res => {
   console.log(res);
 }).catch(err => {
